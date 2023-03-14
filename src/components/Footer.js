8ac7d88540aa2b6dfef3dd6f70491bd22283d307
Tanmay Bhurkunde <tanmaybhurkhunde2018@gmail.com>
2023-03-15 01:11:17 +0530

    build fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import { Box, Typography } from '@mui/material';
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
